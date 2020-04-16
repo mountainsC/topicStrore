ff8e404b19f153cd025891e6c07fe67c60d651a8
chl <maplelucy1991@163.com>
2020-04-16 13:44:13 +0800

    首次提交

--- app/src/main/java/com/geek/test/Solution.java
+++ app/src/main/java/com/geek/test/Solution.java
@@ -1,7 +1,11 @@
 package com.geek.test;
 
+import android.util.Log;
+
+import java.io.Console;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.logging.Logger;
 
 /**
  * @Author: chl
@@ -12,15 +16,29 @@ import java.util.Map;
  * @ModifyContent:
  */
 public class Solution {
+    public static void main(String[] args) {
+        reverse(1534236469);
+    }
+
     public int[] twoSum(int[] nums, int target) {
         Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
             int complement = target - nums[i];
             if (map.containsKey(complement)) {
-                return new int[] { map.get(complement), i };
+                return new int[]{map.get(complement), i};
             }
             map.put(nums[i], i);
         }
         throw new IllegalArgumentException("No two sum solution");
+    }
+
+    public static int reverse(int x) {
+        long n = 0;
+        while (x != 0) {
+            n = n * 10 + x % 10;
+            x = x / 10;
+        }
+        Log.e("666666666","6666666666");
+        return (n < Integer.MIN_VALUE || n > Integer.MAX_VALUE) ? 0 : (int) n;
     }
 }
